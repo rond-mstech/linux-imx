f3f0a495a2bf0cdbf44ecad57262b9907ab33f66
rond-mstech <ron.d@ms-technologies.com>
2023-09-12 10:14:56 +0300

    remove log of every msg

--- drivers/rpmsg/imx_rpmsg_tty.c
+++ drivers/rpmsg/imx_rpmsg_tty.c
@@ -41,8 +41,11 @@ static int rpmsg_tty_cb(struct rpmsg_device *rpdev, void *data, int len,
 
 	dev_dbg(&rpdev->dev, "msg(<- src 0x%x) len %d\n", src, len);
 
+#if 0
+//RD
 	print_hex_dump(KERN_DEBUG, __func__, DUMP_PREFIX_NONE, 16, 1,
 			data, len,  true);
+#endif
 
 	spin_lock_bh(&cport->rx_lock);
 	space = tty_prepare_flip_string(&cport->port, &cbuf, len);
